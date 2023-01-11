50e7da84a5d8906cd7d67ea02f43523ab902b80d
SadiKISLAR <sadi22kislar@gmail.com>
2023-01-11 17:02:56 +0300

    single job is done

--- src/App.js
+++ src/App.js
@@ -30,9 +30,29 @@ function App() {
   const { company, dates, duties, title } = jobs[value]
 
 
-  return <section className='section'>
-    <div className="title">
-    </div></section>
+  return (
+    <section className='section'>
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center"></div>
+      {/*button */}
+      {/*job info */}
+      <article className='job-info'>
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className='job-date'>{dates}</p>
+        {duties.map((duty, index) => {
+          return <div key={index} className="job-desc">
+            <FaAngleDoubleRight className='job-icon'>
+            </FaAngleDoubleRight>
+            <p>{duty}</p>
+          </div>
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App
