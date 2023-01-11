d5501857296addf8f974677587bb42e8aae8ea1c
SadiKISLAR <sadi22kislar@gmail.com>
2023-01-11 14:42:43 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tabs-project'
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [job, setJob] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+  }
+
+
+
+
+  return <h2>tabs project setup</h2>
+}
+
+export default App
