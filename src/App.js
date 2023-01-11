bf3d3b89069f6a09c8205146a3d4d3b49be54f59
SadiKISLAR <sadi22kislar@gmail.com>
2023-01-11 14:47:55 +0300

    fetch is done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
   const [loading, setLoading] = useState(true);
-  const [job, setJob] = useState([]);
+  const [jobs, setJobs] = useState([]);
   const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
     const response = await fetch(url);
     const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+
   }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
 
 
 
